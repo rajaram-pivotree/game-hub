164a52c754f6e5be1b117c412307d722e68dc9a3
rajaram-pivotree <115619197+rajaram-pivotree@users.noreply.github.com>
2023-12-27 02:11:38 +0530

    Display Genres

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../hooks/image-url';
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+          </HStack>
+          <Text fontSize="l">{genre.name}</Text>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
