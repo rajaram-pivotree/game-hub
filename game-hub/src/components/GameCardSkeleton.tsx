03f64fb741963af4dfa31b3b24df485f6f9c6b9f
rajaram-pivotree <115619197+rajaram-pivotree@users.noreply.github.com>
2023-12-26 15:34:43 +0530

    show loading skeletons

---
+++ game-hub/src/components/GameCardSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react';
+import React from 'react';
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <CardBody>
+        <Skeleton height="200px">
+          <SkeletonText></SkeletonText>
+        </Skeleton>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
